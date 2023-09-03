34a984c95171281b604e9bd83e66464d387dad1e
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2023-09-03 14:48:58 -0400

    added helong commments

--- tcpclient/src/main.rs
+++ tcpclient/src/main.rs
@@ -1,4 +1,17 @@
+use std::io::{Read, Write};
 use std::net::TcpStream;
+use std::str;
 fn main() {
-    let _stream = TcpStream::connect("localhost:3000").unwrap();
+    let mut stream = TcpStream::connect("localhost:3000").unwrap();
+    // writes to the stream: convets the string to bytes.
+    stream.write("Hello".as_bytes()).unwrap();
+    // creates a buffer that is able to hold 5 bytes
+    let mut buffer = [0; 5];
+    // reads the buffer and muts it
+    stream.read(&mut buffer).unwrap();
+    println!(
+        "Got response from server: {:?}",
+        // converts from bytes (u8) to string type
+        str::from_utf8(&buffer).unwrap()
+    );
 }
