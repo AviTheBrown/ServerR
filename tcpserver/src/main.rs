2d2e20886698ae559fcc0ce5428d56b351812002
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2023-09-03 14:49:19 -0400

    added comments

--- tcpserver/src/main.rs
+++ tcpserver/src/main.rs
@@ -1,10 +1,17 @@
+use std::io::{Read, Write};
 use std::net::TcpListener;
 
 fn main() {
+    // creates a server
     let connection_listener = TcpListener::bind("127.0.0.1:3000").unwrap();
     println!("Running on port 3000");
+    // continues to execute the server by loop
     for stream in connection_listener.incoming() {
-        let _stream = stream.unwrap();
+        let mut stream = stream.unwrap();
         println!("Connection established");
+        // creates a bufffer that can hold up to 1024 bytes
+        let mut buffer = [0; 1024];
+        stream.read(&mut buffer).unwrap();
+        stream.write(&mut buffer).unwrap();
     }
 }
