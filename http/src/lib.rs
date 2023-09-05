78ad5f2efd49d31338b40273b1bab701fa56bfde
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2023-09-05 04:52:02 -0400

    added DS and Tests

--- http/src/lib.rs
+++ http/src/lib.rs
@@ -15,6 +15,20 @@ impl From<&str> for Method {
         }
     }
 }
+#[derive(Debug, PartialEq)]
+pub enum Version {
+    V1_1,
+    V2_0,
+    Uninitialized,
+}
+impl From<&str> for Version {
+    fn from(vers_type: &str) -> Self {
+        match vers_type {
+            "HTTP/1.1" => Version::V1_1,
+            _ => Version::Uninitialized,
+        }
+    }
+}
 
 #[cfg(test)]
 mod test {
@@ -22,6 +36,14 @@ mod test {
     #[test]
     fn test_method_into() {
         let m: Method = "GET".into();
+        let p: Method = "POST".into();
         assert_eq!(m, Method::GET);
+        assert_eq!(p, Method::POST);
+    }
+    #[test]
+
+    fn test_version_into() {
+        let v: Version = "HTTP/1.1".into();
+        assert_eq!(v, Version::V1_1);
     }
 }
