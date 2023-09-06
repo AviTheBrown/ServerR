801d0058c904f92f9df787205f2d9c3885062b5e
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2023-09-06 16:52:50 -0400

    Added new function and default associated function

--- http/src/httpresponse.rs
+++ http/src/httpresponse.rs
@@ -3,9 +3,50 @@ use std::io::{Result, Write};
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct HttpResponse<'a> {
-	verson: &'a str,
-	status_code: &'s str,
-	status_text: &'a str,
-	headers: Option<HashMap<&'a str, &'a str>,
-	body: Option<String>,
+    verson: &'a str,
+    status_code: &'s str,
+    status_text: &'a str,
+    headers: Option<HashMap<&'a str, &'a str>>,
+    body: Option<String>,
+}
+
+impl<'a> Default for HttpResponse<'a> {
+    fn default() -> Self {
+        Self {
+            version: "HTTP/1.1".into(),
+            status_code: "200".into(),
+            status_text: "OK".into(),
+            headers: None,
+            body: None,
+        }
+    }
+}
+impl<'a> HttpResponse<'a> {
+    fn new(
+        status_code: &'a str,
+        headers: Option<HashMap<&'a str, &'a str>>,
+        body: Option<String>,
+    ) -> HttpResponse<'a> {
+        let mut response: HttpResponse<'a> = HttpResponse::default();
+        if status_code != "200" {
+            response.status_code = status_code.into();
+        };
+        response.headers = match &headers {
+            Some(_h) => headers,
+            None => {
+                let mut h = HashMap::new();
+                h.insert("Content-Type", "text/html");
+                Some(h)
+            }
+        };
+        repsonse.status_code = match &status_code {
+            "200" => "OK".into(),
+            "400" => "Bad Request".into(),
+            "404" => "Not Found".into(),
+            "500" => "Internal Server Error".into(),
+            - => "Not Found".into(),
+        };
+        response.body = body;
+        response
+    }
 }
