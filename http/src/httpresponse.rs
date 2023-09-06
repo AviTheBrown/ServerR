128988ea3385a32c5b65cb7c25aea3306ebc8f7a
Avin Ben-Shemesh <avinbrown.focus@icloud.com>
2023-09-06 19:33:46 -0400

    Added the Fron Impl for HttpResponse

--- http/src/httpresponse.rs
+++ http/src/httpresponse.rs
@@ -3,8 +3,8 @@ use std::io::{Result, Write};
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct HttpResponse<'a> {
-    verson: &'a str,
-    status_code: &'s str,
+    version: &'a str,
+    status_code: &'a str,
     status_text: &'a str,
     headers: Option<HashMap<&'a str, &'a str>>,
     body: Option<String>,
@@ -22,14 +22,14 @@ impl<'a> Default for HttpResponse<'a> {
     }
 }
 impl<'a> HttpResponse<'a> {
-    fn new(
+    pub fn new(
         status_code: &'a str,
         headers: Option<HashMap<&'a str, &'a str>>,
         body: Option<String>,
     ) -> HttpResponse<'a> {
         let mut response: HttpResponse<'a> = HttpResponse::default();
         if status_code != "200" {
-            response.status_code = status_code.into();
+            response.status_code = status_code.;
         };
         response.headers = match &headers {
             Some(_h) => headers,
@@ -39,14 +39,59 @@ impl<'a> HttpResponse<'a> {
                 Some(h)
             }
         };
-        repsonse.status_code = match &status_code {
+        response.status_code = match status_code {
             "200" => "OK".into(),
             "400" => "Bad Request".into(),
             "404" => "Not Found".into(),
             "500" => "Internal Server Error".into(),
-            - => "Not Found".into(),
+            _ => "Not Found".into(),
         };
         response.body = body;
         response
+    }
+    pub fn send_response(&self, write_stream: &mut impl Write) -> Result<()> {
+        let res = self.clone();
+        let response_string: String = String::from(res);
+        let _ = write!(write_stream, "{}", response_string);
+        Ok(())
+    }
+    fn version(&self) -> &str {
+        self.version
+    }
+
+    fn status_code(&self) -> &str {
+        self.status_code
+    }
+
+    fn status_text(&self) -> &str {
+        self.status_text
+    }
+    fn headers(&self) -> String {
+        let hMap: HashMap<&str, &str> = self.headers.clone().unwrap();
+        let mut header_string: String = "".into();
+        for (k, v) in hMap.iter() {
+            header_string = format!("{}{}:{}\r\n", header_string, k, v);
+        }
+        header_string
+    }
+    pub fn body(&self) -> &str {
+        match &self.body {
+            Some(b) => b.as_str(),
+            None => "",
+        }
+    }
+}
+impl<'a> From<HttpResponse<'a>> for String {
+    fn from(res: HttpResponse<'a>) -> Self {
+        let res1 = res.clone();
+        format!(
+            "{}{}{}\r\n{} Content-Lenght: {}\r\n\r\n{}",
+            &res1.version(),
+            &res1.status_code(),
+            &res1.status_text(),
+            &res1.headers(),
+            &res1.body.as_ref().unwrap().len(),
+            &res1.body()
+        )
     }
 }
